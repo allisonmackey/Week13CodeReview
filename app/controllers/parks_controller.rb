e789fac2d1afee6097dfd3bad204171a4ee85c01
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 09:45:58 -0700

    add routes to controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,10 +1,43 @@
 class ParksController < ApplicationController
   def index
     @parks = Park.all
-    json_response(@quotes)
+    json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park, :created)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "This park has been successfully updated"
+      }
+    end
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    if @park.destroy
+      render status: 200, json: {
+        message: "You've successfully deleted this park"
+      }
+    
   end
 
   private
+
+  def park_params
+    params.permit(:name, :address, :website, :phone_number, :open)
+  end
+  
   def json_response(object, status = :ok)
     render json: object, status: status
   end
