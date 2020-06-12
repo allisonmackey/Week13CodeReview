e9a10775d2c6acbe14b90da920cbdba0552ea637
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 11:35:33 -0700

    add test on delete path 'delete park and return correct message' test PASSES

--- spec/requests/delete_parks_spec.rb
+++ spec/requests/delete_parks_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe "delete park route" do
+  before do
+    @test_park = Park.create!({name: "Test Park", address: "Test Address", website: "testwebsite.com", phone_number: "503-123-1234", open: true})
+  end
+
+    it 'will delete a park and return correct message' do
+      delete "/parks/#{@test_park.id}"
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['message']).to eq("You've successfully deleted this park")
+    end
+end
