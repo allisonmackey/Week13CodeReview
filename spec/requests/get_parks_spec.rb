f1d6263f1b9e4a088b640390923b0f2e11c8bbce
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 10:45:48 -0700

    test on specific park route, test PASSES

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -10,4 +10,11 @@ describe 'get all parks route' do
     expect(response).to have_http_status(:success)
   end
 end
+
+describe 'get an individual park route' do
+  it 'returns a park' do
+    get "/parks/400"
+    expect(JSON.parse(response.body)['name']).to eq("Richesse National Park")
+  end
+end
   
