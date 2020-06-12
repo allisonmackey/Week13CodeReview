0eac41ad8d86dcaf4807451d9b2d8443900901b8
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 11:53:59 -0700

    add test on post route 'returns new park object and created status' test PASSES

--- spec/requests/post_parks_spec.rb
+++ spec/requests/post_parks_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
+describe "post park route", :type => :request do
+  before do
+    post '/parks', params: {:name => "test park", :address => "test address", :website => "test website", :phone_number => "111-222-3333", :open => true}
+  end
 
+  it 'returns the new park object and created status' do
+    expect(JSON.parse(response.body)['name']).to eq('test park')
+    expect(response).to have_http_status(:created)
+
+  end
+end
     
