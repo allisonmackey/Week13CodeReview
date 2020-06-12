92995a5cfcea0591f21014ec837629ab4519230d
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 10:41:39 -0700

    move tests to get_parks_spec. test on get route 'returns status code 200' test PASSES

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe 'get all parks route' do
+  before { get '/parks' }
+  it 'returns all parks' do
+    expect(JSON.parse(response.body).size).to eq(200)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
+  
