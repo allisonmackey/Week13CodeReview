848986802746a4c0a133e6cbe6c92aac92c8364d
Allison Mackey <a.mackey693@gmail.com>
2020-06-12 11:43:09 -0700

    add test on patch path 'updates a park and returns correct message' test PASSES

--- spec/requests/delete_parks_spec.rb
+++ spec/requests/delete_parks_spec.rb
@@ -5,17 +5,17 @@ describe "delete park route" do
     @test_park = Park.create!({name: "Test Park", address: "Test Address", website: "testwebsite.com", phone_number: "503-123-1234", open: true})
   end
 
-    it 'will delete a park and return correct message' do
-      delete "/parks/#{@test_park.id}"
-      expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)['message']).to eq("You've successfully deleted this park")
-    end
+  it 'will delete a park and return correct message' do
+    delete "/parks/#{@test_park.id}"
+    expect(response).to have_http_status(200)
+    expect(JSON.parse(response.body)['message']).to eq("You've successfully deleted this park")
+  end
 
-    it 'will return an error message if user deletes an item that does not exist' do
-      delete "/parks/#{@test_park.id}"
-      # have to run delete twice do to the before block
-      delete "/parks/#{@test_park.id}"
-      expect(response).to have_http_status(404)
-      expect(JSON.parse(response.body)['message']).to eq("Couldn't find Park with 'id'=#{@test_park.id}")
-    end
+  it 'will return an error message if user deletes an item that does not exist' do
+    delete "/parks/#{@test_park.id}"
+    # have to run delete twice do to the before block
+    delete "/parks/#{@test_park.id}"
+    expect(response).to have_http_status(404)
+    expect(JSON.parse(response.body)['message']).to eq("Couldn't find Park with 'id'=#{@test_park.id}")
+  end
 end
--- spec/requests/patch_parks_spec.rb
+++ spec/requests/patch_parks_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe 'update park route' do
+  before do
+    @test_park = Park.create!({name: "Test Park", address: "Test Address", website: "testwebsite.com", phone_number: "503-123-1234", open: true})
+  end
+
+  it 'will update a park and return correct message' do
+    patch "/parks/#{@test_park.id}", params: {name: "Test Update"}
+    expect(JSON.parse(response.body)['message']).to eq("This park has been successfully updated")
+    expect(response).to have_http_status(200)
+  end
+end
