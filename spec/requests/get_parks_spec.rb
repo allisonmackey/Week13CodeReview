859411d700de955814cf6f45a64494a53ca54e13
Allison Mackey <a.mackey693@gmail.com>
2020-06-21 12:45:02 -0700

    add logic for tests to pass without seeding database

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'get all parks route' do
   before { get '/parks' }
   it 'returns all parks' do
-    expect(JSON.parse(response.body).size).to eq(200)
+    expect(JSON.parse(response.body).size).to eq(Park.all.count)
   end
 
   it 'returns status code 200' do
@@ -13,8 +13,9 @@ end
 
 describe 'get an individual park route' do
   it 'returns a park' do
-    get "/parks/400"
-    expect(JSON.parse(response.body)['name']).to eq("Richesse National Park")
+    @test_park = Park.create!({name: "Another Test Park", address: "Test Address", website: "testwebsite.com", phone_number: "503-123-1234", open: true})
+    get "/parks/#{@test_park.id}"
+    expect(JSON.parse(response.body)['name']).to eq("Another Test Park")
   end
 
   it 'returns an error when fetching a park that does not exist' do
